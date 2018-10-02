bd94ab305d896674818955004206cb4f61598ce1
Brian Birdwell <bbirdwell@gmail.com>
2018-10-02 16:31:39 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,6 +15,6 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts[:Freddy_Mercury][:flavors]
-  
+  contacts[:Freddy_Mercury][:favorite_ice_cream_flavors].delete("strawberry")
+  contacts
 end
